651ffcbe0f318f4b728b1752bdd62511fc0eea41
Manuel <manuel.richter@posteo.de>
2017-02-27 00:15:13 +0000

    Downgrade forge and bump version to 1.8.9-2.6.0.0
    Verison format in compliance with
    https://mcforge.readthedocs.io/en/latest/conventions/versioning/

--- src/main/java/de/tennoxlab/mods/bacteria/Bacteria.java
+++ src/main/java/de/tennoxlab/mods/bacteria/Bacteria.java
@@ -20,7 +20,7 @@ import java.util.List;
 @Mod(modid = Bacteria.MODID, version = Bacteria.VERSION)
 public class Bacteria {
 	public static final String MODID = "tennox_bacteria";
-	public static final String VERSION = "1.9.4-3.0.0";
+	public static final String VERSION = "1.8.9-2.6.0.0";
 
 	@Mod.Instance
 	public static Bacteria instance;
